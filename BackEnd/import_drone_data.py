871739e5863d08db6b00df52795324f2b88e93e5
Alice Yu <aljyu.95@gmail.com>
2017-01-21 22:24:56 -0800

    Update import_drone_data.py

--- BackEnd/import_drone_data.py
+++ BackEnd/import_drone_data.py
@@ -2,32 +2,55 @@
 
 #from pexpect import pxssh
 #import pysftp
+import paramiko
 import os
 import csv
+import sys
+import time
 
 DRONE_CONTENT = []
 
 # SSH into Drone
 def ssh_drone():
-    HOST = "xxx"
-    USER = "xxx"
-    PASS = "xxx"
-    global DRONE_CONTENT
- #   s = pxssh.pxssh()
- #   if not s.login(HOST, USER, PASS):
- #       s.sendline('ls -l')
- #       s.prompt()
- #       print s.before
- #       s.logout()
- #   with pysftp.Connection(HOST, username=USER, password=PASS) as sftp:
- #       try:
- #           sftp.listdir()
- #       except UnicodeEncodeError as e:
- #           print e
-       # with sftp.cd('public'):             # temporarily chdir to public
-       #     sftp.put('/my/local/filename')  # upload file to public/ on remote
-       #     sftp.get('remote_file')         # get a remote file
-    DRONE_CONTENT = CONTENT
+    HOST = "unix.ucsc.edu"
+    USER = ""
+    PASS = ""
+    PORT = 22
+    # Try to connect to the host.
+    # Retry a few times if it fails.
+    i = 1
+    while True:
+        print "Trying to connect to %s (%i/30)" % (HOST, i)
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(HOST, port=PORT, username=USER, password=PASS)
+            print "Connected to %s" % host
+            break
+        except paramiko.AuthenticationException:
+            print "Authentication failed when connecting to %s" % HOST
+            sys.exit(1)
+        except:
+            print "Could not SSH to %s, waiting for it to start" % HOST
+            i += 1
+            time.sleep(2)
+        # If we could not connect within time limit
+        if i == 30:
+            print "Could not connect to %s. Giving up" % HOST
+            sys.exit(1)    
+    # Send the command (non-blocking)
+    stdin, stdout, stderr = ssh.exec_command("ls -l")
+    # Wait for the command to terminate
+    while not stdout.channel.exit_status_ready():
+        # Only print data if there is data to read in the channel
+        if stdout.channel.recv_ready():
+            rl, wl, xl = select.select([stdout.channel], [], [], 0.0)
+            if len(rl) > 0:
+                # Print data from stdout
+                print stdout.channel.recv(1024),
+    # Disconnect from the host
+    print "Command done, closing SSH connection"
+    ssh.close()        
 
 # Retrieve MAC Address from Drone
 def retrieve_mac():
@@ -76,8 +99,9 @@ def Main():
     # 4. Dump contents into local device
     # 5. Connect to App Engine SQL
     # 6. Store into App Engine SQL
-    drone_content = [['45ADGS324D', '34242','54325','Found'],['45FDSS324D', '3423212','5442342325','Not Found']]
-    store_local_db(drone_content)
+    #drone_content = [['45ADGS324D', '34242','54325','Found'],['45FDSS324D', '3423212','5442342325','Not Found']]
+    #store_local_db(drone_content)
+    ssh_drone()
 
 if __name__=="__main__":
     Main()
